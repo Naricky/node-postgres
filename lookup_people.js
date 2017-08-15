358bc36e66efc42aafb428cc3e06aa266854b1b5
Ricky <Na_ricky@hotmail.com>
2017-08-15 16:06:43 +0000

    1st asgnment finished

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+var args = process.argv.slice(2)
 
 const client = new pg.Client({
   user     : settings.user,
@@ -10,15 +11,19 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+const Name = args[0];
+
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query("SELECT * FROM famous_people WHERE last_name = $1::varchar or first_name =$1::varchar", [Name], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log("Searching...")
+    console.log("Found " + result.rows.length + " person(s) by the name " + Name);
+    console.log(result.rows[0].id, ":", result.rows[0].first_name, result.rows[0].last_name, ",born ", result.rows[0].birthdate)
     client.end();
   });
 });
